e8b0b960c47195ff940e31b74ed306b9172eba4a
hazal ilik <ilikhazal16@gmail.com>
2025-07-18 12:17:09 +0300

    Add CalculatorModel class for state management.
    Updated to include properties, each one has been initialized with default values to  manage the calculator's state effectively.

---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public string CurrentInput { get; set; } = ""; // initially empty
+        public string PreviousInput { get; set; } = ""; // initially empty
+        public string Operation { get; set; } = ""; // initially no operation
+        public double Result { get; set; } = 0.0; 
+        public bool ResetInput { get; set; } = false; 
+    }
+}
