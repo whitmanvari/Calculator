e0f7a445ccb050f0f3a344d89feac46d8d156826
hazal ilik <ilikhazal16@gmail.com>
2025-07-18 12:44:15 +0300

    Refactor operations classes to public and enhance functionality Added static methods for trigonometric calculations.

--- Calculator/Operations/BasicOperations.cs
+++ Calculator/Operations/BasicOperations.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Calculator.Operations
 {
-    internal class BasicOperations
+    public class BasicOperations : IOperations
     {
+        public double Calculate(double num1, double num2)
+        {
+            switch(Operation)
+            {
+                case "+": return num1 + num2;
+                case "-": return num1 - num2;
+                case "x": return num1 * num2;
+                case "/": 
+                if(num2 != 0) return num1 / num2; 
+                else 
+                throw new DivideByZeroException("Cannot divide by zero.");
+                default: return num2; // If no operation is specified, return the second number
+            }
+        }
     }
 }
--- Calculator/Operations/TrigonometricOperations.cs
+++ Calculator/Operations/TrigonometricOperations.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Calculator.Operations
 {
-    internal class TrigonometricOperations
+    public class TrigonometricOperations
     {
+        public static double Sin(double num) => Math.Sin(num);
+        public static double Cos(double num) => Math.Cos(num);
+        public static double Tan(double num) => Math.Tan(num);
+        public static double Csc(double num) => 1 / Math.Sin(num);
+        public static double Sec(double num) => 1 / Math.Cos(num);
+        public static double Cot(double num) => 1 / Math.Tan(num);
+        
+
     }
 }
