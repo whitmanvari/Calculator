11af24dde0fdf6ecdc8a6a1b97bb227a1d3c44bd
hazal ilik <ilikhazal16@gmail.com>
2025-07-18 12:35:38 +0300

    Add operations and interface for calculator functionality
    Updated project to include new files such as:
    - `BasicOperations.cs`: Added internal class for basic operations.
    - `IOperations.cs`: Defined public interface with a `Calculate` method.
    - `TrigonometricOperations.cs`: Added public class for trigonometric operations.

---
+++ Calculator/Operations/IOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Operations
+{
+    public interface IOperations
+    {
+        double Calculate(double num1, double num2);
+    }
+}
